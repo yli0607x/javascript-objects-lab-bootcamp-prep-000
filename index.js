016738026c61d65bb21fa0d5e535b5ee502d1f6b
Yu Li <yli0607x@gmail.com>
2018-08-07 02:24:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function deleteFromObjectByKey(object,key){
   delete newobj[key]
   return newobj
 }
+
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key]
+  return object
+}
